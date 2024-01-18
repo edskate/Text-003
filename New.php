5af2f3464654aa69e703a919dd0251b68c22636b
Edilson gomes de lima <edskateicevida@gmail.com>
2024-01-17 23:10:34 -0300

    add style

--- New.php
+++ New.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New text php.</title>
+    <style>
+        body{
+            margin: 50px ;
+            background-color: #c0c0c0;
+        }
+    </style>
 </head>
 <body>
     <h1>New text php.</h1>
