4457fb6f201aa2f798af1c28fc7804d9948f5e5a
Edilson gomes de lima <edskateicevida@gmail.com>
2024-01-17 22:57:43 -0300

    first commit

---
+++ text.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: #004080;
+            text-shadow: 10px 10px 10px #000;
+            margin: 50px;
+        }
+    </style>
+</head>
+<body>
+<?php
+// Programa PHP simples para exibir uma mensagem na tela
+$mensagem = "<h1>Olá, Mundo!</h1>";
+echo "<h1>$mensagem</h1>";
+?>
+
+</body>
+</html>
+
